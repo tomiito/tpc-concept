079a597ef956fd538d7ffc4f93678c8870e616e6
TOMAS DE PRIEDE <tomasdepriede@gmail.com>
2019-01-17 16:10:34 -0300

    Pushing compiled code for github pages.

--- docs/assets/scripts/angular-material-boilerplate.js
+++ docs/assets/scripts/angular-material-boilerplate.js
@@ -1,6 +1,252 @@
 /**
  * angular-material-boilerplate v1.0.0
- * Copyright (c) 2017 Cathal Mac Donnacha
+ * Copyright (c) 2019 Cathal Mac Donnacha
  * Licensed under MIT
  */
-angular.module("angular-material-boilerplate",["ngAnimate","ngAria","ngMaterial","ui.router","ngResource","templates","app.shared","app.layout","app.toolbar","app.sidenav","app.todo","app.about"]).config(["$mdThemingProvider",function(t){t.theme("default").primaryPalette("indigo").accentPalette("purple",{default:"200"})}]).run(["$state",function(t){t.go("todo")}]),angular.module("about.controllers.AboutCtrl",[]).controller("AboutCtrl",function(){var t=this;t.message="And here is where I tell you all about myself."}),angular.module("app.about",["ui.router","about.controllers.AboutCtrl"]).config(["$stateProvider",function(t){t.state("about",{url:"/about",templateUrl:"about/about.view.html"})}]),angular.module("app.shared",["shared.directives.backButton","shared.services.Utils"]),angular.module("layout.controllers.LayoutCtrl",[]).controller("LayoutCtrl",function(){}),angular.module("app.layout",["ui.router","layout.controllers.LayoutCtrl"]).config(["$stateProvider",function(t){t.state("layout",{url:"/layout",templateUrl:"layout/layout.view.html"})}]),angular.module("todo.controllers.ToDoCtrl",[]).controller("ToDoCtrl",["ToDo",function(t){var e=this;e.toDoItems=t.getItems}]),angular.module("app.todo",["ui.router","todo.controllers.ToDoCtrl","todo.services.ToDo"]).config(["$stateProvider",function(t){t.state("todo",{url:"/todo",templateUrl:"todo/todo.view.html"})}]),angular.module("todo.services.ToDo",[]).factory("ToDo",function(){var t=[{title:"Make a todo list",completed:!0},{title:"Check off first item on todo list",completed:!0},{title:"Realise you've have already accomplished two things on the list",completed:!0},{title:"Reward yourself with a nice long nap",completed:!1}],e={getItems:t};return e}),angular.module("sidenav.controllers.SideNavCtrl",[]).controller("SideNavCtrl",["$mdSidenav","$state","SideNav",function(t,e,o){function l(t){a.selected=t,r(),e.go(t.route)}function r(){t("left").close()}var a=this;a.selectMenuItem=l,a.closeSideNav=r,a.menuItems=o.getMenuItems,a.selected=a.menuItems[0]}]),angular.module("app.sidenav",["sidenav.controllers.SideNavCtrl","sidenav.services.SideNav"]),angular.module("sidenav.services.SideNav",[]).factory("SideNav",function(){var t=[{name:"To-Do",icon:"",route:"todo"},{name:"About",icon:"",route:"about"}],e={getMenuItems:t};return e}),angular.module("toolbar.controllers.ToolbarCtrl",[]).controller("ToolbarCtrl",["$state","$scope","$mdSidenav",function(t,e,o){e.openSideNav=function(){o("left").open()}}]),angular.module("app.toolbar",["toolbar.controllers.ToolbarCtrl"]),angular.module("shared.services.Utils",[]).factory("Utils",function(){function t(t){return!(null!==t&&!angular.isUndefined(t))}function e(t){return!!(angular.isUndefined(t)||t.trim().length<=0)}function o(t){return null===t||!!(angular.isUndefined(t)||t.trim().length<=0)}function l(t){return null===t||t.trim().length<=0}var r={isNullOrUndefined:t,isUndefinedOrWhitespace:e,isNullOrWhitespace:l,isNullOrUndefinedOrWhitespace:o};return r}),angular.module("shared.directives.backButton",[]).directive("backButton",["$window",function(t){return{restrict:"A",link:function(e,o){o.bind("click",function(){t.history.back()})}}}]);
+angular.module('angular-material-boilerplate', [
+  /**
+   * Dependencies must be injected in specific order:
+   * 1) AngularJS dependencies
+   * 2) Compiled HTML templates
+   * 3) Common Services, Directives, Filters and Utilities
+   * 4) Main App Layout
+   * 5) Other App components (e.g. Toolbar, About, etc)
+   */
+
+  // AngularJS dependencies
+  'ngAnimate',
+  'ngAria',
+  'ngMaterial',
+  'ui.router',
+  'ngResource',
+
+  // Include compiled HTML templates
+  'templates',
+
+  // Common/shared code
+  'app.shared',
+
+  // Layout
+  'app.layout',
+
+  // Components
+  'app.toolbar',
+  'app.sidenav',
+  'app.todo',
+  'app.about'
+])
+
+.config(["$mdThemingProvider", function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('indigo')
+    .accentPalette('purple', {
+      'default': '200'
+    });
+}])
+
+.run(['$state', function ($state) {
+  $state.go('todo');
+}]);
+
+angular.module('about.controllers.AboutCtrl', [])
+  .controller('AboutCtrl', function() {
+    var vm = this;
+    vm.message = 'And here is where I tell you all about myself.';
+  });
+
+angular.module('app.about', [
+  'ui.router',
+  'about.controllers.AboutCtrl'
+])
+
+.config(["$stateProvider", function config($stateProvider) {
+  $stateProvider.state('about', {
+    url: '/about',
+    templateUrl: 'about/about.view.html'
+  });
+}]);
+
+angular.module('layout.controllers.LayoutCtrl', [])
+  .controller('LayoutCtrl', function() {
+
+  });
+
+angular.module('app.layout', [
+  'ui.router',
+  'layout.controllers.LayoutCtrl'
+])
+
+.config(["$stateProvider", function config($stateProvider) {
+  $stateProvider.state('layout', {
+    url: '/layout',
+    templateUrl: 'layout/layout.view.html'
+  });
+}]);
+
+angular.module('app.shared', [
+  'shared.directives.backButton',
+  'shared.directives.courseCard',
+  'shared.services.Utils'
+]);
+
+angular.module('sidenav.controllers.SideNavCtrl', [])
+  .controller('SideNavCtrl', ["$mdSidenav", "$state", "SideNav", function($mdSidenav, $state, SideNav) {
+    var vm = this;
+    vm.selectMenuItem = selectMenuItem;
+    vm.closeSideNav = closeSideNav;
+    vm.menuItems = SideNav.getMenuItems;
+    vm.selected = vm.menuItems[0];
+
+    function selectMenuItem(menuItem) {
+      vm.selected = menuItem;
+      closeSideNav();
+      $state.go(menuItem.route);
+    }
+
+    function closeSideNav() {
+      $mdSidenav('left').close();
+    }
+  }]);
+
+angular.module('app.sidenav', [
+  'sidenav.controllers.SideNavCtrl',
+  'sidenav.services.SideNav'
+]);
+
+
+angular.module('sidenav.services.SideNav', [])
+  .factory('SideNav', function() {
+
+    var menuItems = [
+      {
+        name: 'To-Do',
+        icon: '',
+        route: 'todo'
+      },
+      {
+        name: 'About',
+        icon: '',
+        route: 'about'
+      }
+    ];
+
+    var service = {
+      getMenuItems: menuItems
+    };
+    return service;
+
+  });
+
+angular.module('toolbar.controllers.ToolbarCtrl', [])
+  .controller('ToolbarCtrl', ["$state", "$scope", function($state, $scope) {
+  }]);
+
+angular.module('app.toolbar', [
+  'toolbar.controllers.ToolbarCtrl'
+]);
+
+angular.module('todo.controllers.ToDoCtrl', [])
+  .controller('ToDoCtrl', ["ToDo", function(ToDo) {
+    var vm = this;
+    vm.toDoItems = ToDo.getItems;
+
+  }]);
+
+angular.module('app.todo', [
+  'ui.router',
+  'todo.controllers.ToDoCtrl',
+  'todo.services.ToDo'
+])
+
+.config(["$stateProvider", function config($stateProvider) {
+  $stateProvider.state('todo', {
+    url: '/todo',
+    templateUrl: 'todo/todo.view.html'
+  });
+}]);
+
+angular.module('todo.services.ToDo', [])
+  .factory('ToDo', function() {
+
+    var toDoItems = [
+      {
+        title:  'Make a todo list',
+        completed: true
+      },
+      {
+        title:  'Check off first item on todo list',
+        completed: true
+      },
+      {
+        title:  'Realise you\'ve have already accomplished two things on the list',
+        completed: true
+      },
+      {
+        title:  'Reward yourself with a nice long nap',
+        completed: false
+      }
+    ];
+
+    var service = {
+      getItems: toDoItems
+    };
+    return service;
+
+  });
+
+angular.module('shared.directives.backButton', [])
+  .directive('backButton', ['$window', function($window) {
+    return {
+      restrict: 'A',
+      link: function (scope, elem) {
+        elem.bind('click', function () {
+          $window.history.back();
+        });
+      }
+    };
+  }]);
+
+angular.module('shared.services.Utils', [])
+  .factory('Utils', function Utils() {
+
+    var service = {
+      isNullOrUndefined: isNullOrUndefined,
+      isUndefinedOrWhitespace: isUndefinedOrWhitespace,
+      isNullOrWhitespace: isNullOrWhitespace,
+      isNullOrUndefinedOrWhitespace: isNullorUndefinedOrWhitespace
+    };
+    return service;
+
+    function isNullOrUndefined(object) {
+      return object === null || angular.isUndefined(object) ? true : false;
+    }
+
+    function isUndefinedOrWhitespace(stringText) {
+      return angular.isUndefined(stringText) || stringText.trim().length <= 0 ? true : false;
+    }
+
+    function isNullorUndefinedOrWhitespace(stringText) {
+      if(stringText !== null) {
+        return angular.isUndefined(stringText) || stringText.trim().length <= 0 ? true : false;
+      } else {
+        return true;
+      }
+    }
+
+    function isNullOrWhitespace(stringText) {
+      return stringText === null || stringText.trim().length <= 0 ? true : false;
+    }
+  });
+
+angular.module('shared.directives.courseCard', [])
+  .directive('courseCard', ['$window', function($window) {
+    return {
+      restrict: 'E',
+      templateUrl: 'shared/directives/course-card/course-card.view.html',
+      link: function (scope, elem) {
+        elem.bind('click', function () {
+          $window.history.back();
+        });
+      }
+    };
+  }]);
--- gulp_config.js
+++ gulp_config.js
@@ -8,41 +8,41 @@ module.exports = {
       '!src/app/**/*.spec.js',
       '!src/app/**/*.e2e.js'
     ],
-    destinationFolder: isProduction ? 'production/assets/scripts' : 'public/assets/scripts',
+    destinationFolder: isProduction ? 'production/assets/scripts' : 'docs/assets/scripts',
     destinationName: pkg.name + '.js'
   },
   sass: {
     sources : ['src/app/**/*.scss'],
-    destinationFolder: isProduction ? 'production/assets/css' : 'public/assets/css',
+    destinationFolder: isProduction ? 'production/assets/css' : 'docs/assets/css',
     destinationName: pkg.name + '.css'
   },
   browserify: {
     sources : ['./vendor_files.js'],
-    destinationFolder: isProduction ? 'production/assets/scripts' : 'public/assets/scripts',
+    destinationFolder: isProduction ? 'production/assets/scripts' : 'docs/assets/scripts',
     destinationName: 'bundle.js'
   },
   templates: {
     sources : ['src/app/**/*.html'],
-    destinationFolder: isProduction ? 'production/assets/scripts' : 'public/assets/scripts',
+    destinationFolder: isProduction ? 'production/assets/scripts' : 'docs/assets/scripts',
   },
   images: {
     sources : ['src/assets/images/*'],
-    destinationFolder : isProduction ? 'production/assets/images' : 'public/assets/images'
+    destinationFolder : isProduction ? 'production/assets/images' : 'docs/assets/images'
   },
   vendor: {
     sources : ['node_modules/angular-material/angular-material.min.css'],
-    destinationFolder: isProduction ? 'production/assets/css' : 'public/assets/css',
+    destinationFolder: isProduction ? 'production/assets/css' : 'docs/assets/css',
   },
   assets: {
     sources : ['src/assets/images/favicon.ico'],
-    destinationFolder : isProduction ? 'production/assets/images' : 'public/assets/images'
+    destinationFolder : isProduction ? 'production/assets/images' : 'docs/assets/images'
   },
   index: {
     sources : ['src/index.html'],
-    destinationFolder : isProduction ? 'production' : 'public'
+    destinationFolder : isProduction ? 'production' : 'docs'
   },
   serve: {
-    baseDir : isProduction ? 'production' : 'public',
+    baseDir : isProduction ? 'production' : 'docs',
     port : isProduction ? 4000 : 3000
   },
   coverage: {
