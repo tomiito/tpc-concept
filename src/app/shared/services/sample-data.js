c8dcfb3702a8b5b7e2cf665b82cefb7e9605b0c1
TOMAS DE PRIEDE <tomasdepriede@gmail.com>
2019-01-17 19:24:54 -0300

    Push main items.

--- src/app/shared/services/sample-data.js
+++ src/app/shared/services/sample-data.js
@@ -3,70 +3,70 @@ angular.module('shared.services.sampleData', [])
 
     var sampleMain = [{
       title: 'Become a PHP Master and Make Money Fast',
-      pictureUrl: '/assets/images/demo-course-1.jpg'
+      pictureUrl: 'assets/images/demo-course-1.jpg'
     },
       {
         title: 'Why I Use Both WebStorm and Sublime Text',
-        pictureUrl: '/assets/images/demo-course-2.jpg'
+        pictureUrl: 'assets/images/demo-course-2.jpg'
       },
       {
         title: 'Mastering Refactoring',
-        pictureUrl: '/assets/images/demo-course-3.jpg'
+        pictureUrl: 'assets/images/demo-course-3.jpg'
       },
       {
         title: 'The future of Web Design',
-        pictureUrl: '/assets/images/demo-course-5.jpg'
+        pictureUrl: 'assets/images/demo-course-5.jpg'
       },
       {
         title: 'Tips for being better Musician',
-        pictureUrl: '/assets/images/demo-course-6.jpg'
+        pictureUrl: 'assets/images/demo-course-6.jpg'
       }
     ];
 
 
     var sampleData = [{
       title: 'Become a PHP Master and Make Money Fast',
-      pictureUrl: '/assets/images/demo-course-1.jpg',
+      pictureUrl: 'assets/images/demo-course-1.jpg',
       reviews: 3
     },
       {
         title: 'Why I Use Both WebStorm and Sublime Text',
-        pictureUrl: '/assets/images/demo-course-2.jpg',
+        pictureUrl: 'assets/images/demo-course-2.jpg',
         reviews: 0
       },
       {
         title: 'Mastering Refactoring',
-        pictureUrl: '/assets/images/demo-course-3.jpg',
+        pictureUrl: 'assets/images/demo-course-3.jpg',
         reviews: 4
       },
       {
         title: 'How to get better at Learning',
-        pictureUrl: '/assets/images/demo-course-4.jpg',
+        pictureUrl: 'assets/images/demo-course-4.jpg',
         reviews: 2
       },
       {
         title: 'The future of Web Design',
-        pictureUrl: '/assets/images/demo-course-5.jpg',
+        pictureUrl: 'assets/images/demo-course-5.jpg',
         reviews: 1
       },
       {
         title: 'Tips for being better Musician',
-        pictureUrl: '/assets/images/demo-course-6.jpg',
+        pictureUrl: 'assets/images/demo-course-6.jpg',
         reviews: 5
       }
     ];
 
     var sampleNews = [{
       title: 'Become a PHP Master and Make Money Fast',
-      pictureUrl: '/assets/images/demo-course-1.jpg'
+      pictureUrl: 'assets/images/demo-course-1.jpg'
     },
       {
         title: 'Why I Use Both WebStorm and Sublime Text',
-        pictureUrl: '/assets/images/demo-course-2.jpg'
+        pictureUrl: 'assets/images/demo-course-2.jpg'
       },
       {
         title: 'Mastering Refactoring',
-        pictureUrl: '/assets/images/demo-course-3.jpg'
+        pictureUrl: 'assets/images/demo-course-3.jpg'
       }
     ];
 
